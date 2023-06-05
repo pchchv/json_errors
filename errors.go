427a4b373595e69adacfcd2ec6087b7857dbfc8a
pchchv <ipchchv@gmail.com>
2023-06-06 00:48:00 +0300

    Implement a BaseError interface

--- errors.go
+++ errors.go
@@ -1,1 +1,7 @@
 package json_errors
+
+// BaseError interface reveals additional information about the error.
+// Implements a built-in error interface.
+type BaseError interface {
+	Error() string
+}
